de0dc82a7e3627434ac128a57db660e047fa283a
Jani Piirainen <piirainen.jani98@gmail.com>
2026-03-06 10:33:55 +0200

    Exercise 9.18 done

--- flight-diary-frontend/src/App.tsx
+++ flight-diary-frontend/src/App.tsx
@@ -1,33 +1,79 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-import type { DiaryEntry } from "./types";
+import type { DiaryEntry, NewDiaryEntry } from "./types";
+import { getAllDiaries, createDiary } from "./services/diaryService";
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
 
+  const [date, setDate] = useState("");
+  const [weather, setWeather] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const [comment, setComment] = useState("");
+
   useEffect(() => {
-    const fetchDiaries = async () => {
-      try {
-        const response = await axios.get<DiaryEntry[]>(
-          "http://localhost:3000/api/diaries/full",
-        );
-        setDiaries(response.data);
-      } catch (error) {
-        console.error("Failed to fetch diaries:", error);
-      }
-    };
-    fetchDiaries();
+    getAllDiaries().then((data) => {
+      setDiaries(data);
+    });
   }, []);
+
+  const addDiary = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    const newEntry: NewDiaryEntry = {
+      date,
+      weather,
+      visibility,
+      comment,
+    };
+
+    const returnedDiary = await createDiary(newEntry);
+    setDiaries(diaries.concat(returnedDiary));
+
+    setDate("");
+    setWeather("");
+    setVisibility("");
+    setComment("");
+  };
 
   return (
     <div>
-      <h1>Flight Diaries</h1>
+      <h1>Add new entry</h1>
+
+      <form onSubmit={addDiary}>
+        <div>
+          date
+          <input value={date} onChange={(e) => setDate(e.target.value)} />
+        </div>
+
+        <div>
+          weather
+          <input value={weather} onChange={(e) => setWeather(e.target.value)} />
+        </div>
+
+        <div>
+          visibility
+          <input
+            value={visibility}
+            onChange={(e) => setVisibility(e.target.value)}
+          />
+        </div>
+
+        <div>
+          comment
+          <input value={comment} onChange={(e) => setComment(e.target.value)} />
+        </div>
+
+        <button type="submit">add</button>
+      </form>
+
+      <h2>Diary entries</h2>
+
       {diaries.map((diary) => (
-        <div key={diary.id} style={{ marginBottom: "1em" }}>
-          <p>{diary.date}</p>
-          <p>Weather: {diary.weather}</p>
-          <p>Visibility: {diary.visibility}</p>
-          <p>Comment: {diary.comment}</p>
+        <div key={diary.id}>
+          <h3>{diary.date}</h3>
+          <p>weather: {diary.weather}</p>
+          <p>visibility: {diary.visibility}</p>
+          <p>{diary.comment}</p>
         </div>
       ))}
     </div>
