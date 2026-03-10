275273049899a3c05481dcee031f807c96a8f81c
Jani Piirainen <piirainen.jani98@gmail.com>
2026-03-10 09:46:00 +0200

    Exercise 9.23 and 9.24 done

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -1,5 +1,3 @@
-// src/types.ts
-
 export interface Diagnosis {
   code: string;
   name: string;
@@ -12,15 +10,59 @@ export enum Gender {
   Other = "other",
 }
 
-export interface Entry {}
+/* ---------- ENTRY TYPES ---------- */
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export enum HealthCheckRating {
+  Healthy = 0,
+  LowRisk = 1,
+  HighRisk = 2,
+  CriticalRisk = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+/* ---------- PATIENT TYPES ---------- */
 
 export interface Patient {
   id: string;
   name: string;
-  dateOfBirth: string;
   ssn: string;
-  gender: Gender;
   occupation: string;
+  gender: Gender;
+  dateOfBirth: string;
   entries: Entry[];
 }
 
